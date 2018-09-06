3d49eec389487ea47d2b84aa982813059c904642
gee_li <geedyne.li@gmail.com>
2018-09-06 13:05:44 -0500

    finish basic requirements with pair Partner

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var elementsWithClass = [];
+
+  var findElements = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+    //console.log('this is our element: ', element.classList)
+      elementsWithClass.push(element)
+    }
+
+    if (element.childNodes) {
+      for ( var i = 0; i < element.childNodes.length; i++ ) {
+        findElements(element.childNodes[i]);
+      };
+    }
+  };
+
+  findElements(document.body);
+
+  return elementsWithClass;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+// input: JS method/function
+// output: the JS method in string form
+
+  //console.log(JSON.stringify({ x: 5, y: 6 }));
+  // expected output: "{"x":5,"y":6}"
+
+
+
+var stringifyJSON = function(obj){
+    //boolean, null and numbers
+      //we want to add '' on the outside of number value
+    if( typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+      return '' + obj
+    }
+    //string
+      //we want to add '' on the outside && change inside quotes to ""
+    if( typeof obj === 'string') {
+      return '"' + obj + '"'
+    }
+    if(Array.isArray(obj) && obj) {
+        var resultArr = [];
+      for (var i = 0; i < obj.length; i++) {
+        resultArr.push(stringifyJSON(obj[i]));
+      }
+      return '[' + resultArr + ']'
+      }
+
+    if(typeof obj === 'object' && obj) {
+      var resultObj = [];
+      for(var key in obj) {
+        if(obj[key] === undefined || typeof obj[key] === 'function'){
+          continue;
+        }
+        resultObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+      return '{' + resultObj.join(',') + '}';
+    }
 };
+
+
+
+
